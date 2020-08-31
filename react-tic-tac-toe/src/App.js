45c3e3f26b9f8e460f5872e2e303cf99d05219b5
Irene <airine@Irenes-MacBook-Air.local>
2020-08-31 00:01:28 -0700

    [Components] Add 3 components and game is alive

---
+++ react-tic-tac-toe/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Square from './components/Square';
+import Board from './components/Board';
+import Game from './components/Game';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <main>
+        <div>
+          
+          <Game />
+          
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+
+
+export default App;
